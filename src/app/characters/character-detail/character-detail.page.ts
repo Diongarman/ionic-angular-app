e124177fdca61ec71d275982c441ae12886cdd1f
Dion Garman <diongarman@gmail.com>
2019-12-07 18:07:27 +0000

    Setting up Character Details - injected character service into character-detail.page component

--- src/app/characters/character-detail/character-detail.page.ts
+++ src/app/characters/character-detail/character-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CharactersService } from '../characters.service';
+import { Character } from '../character.model';
 
 @Component({
   selector: 'app-character-detail',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterDetailPage implements OnInit {
 
-  constructor() { }
+  loadedCharacter: Character;
+
+  constructor(private activatedRoute: ActivatedRoute, private characterService: CharactersService) { }
 
   ngOnInit() {
+    // activatedRoute is an 'observable'
+    // an observable is an object you can 'subscribe' to to get data
+    // Find out more
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      if (!paramMap.has('characterId')) {
+        // redirect
+        return;
+      }
+      const charId = paramMap.get('characterId');
+      this.loadedCharacter = this.characterService.getCharacter(charId)
+      
+    })
   }
 
 }
