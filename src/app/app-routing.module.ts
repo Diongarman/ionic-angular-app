b7892597155772c3138418cb5f96b37863127dfa
Dion Garman <diongarman@gmail.com>
2019-12-08 18:51:26 +0000

    Setting up auth page: generated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
     path: 'characters',
     loadChildren: () => import('./characters/characters.module').then( m => m.CharactersPageModule)
   },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
 ];
 
 @NgModule({
