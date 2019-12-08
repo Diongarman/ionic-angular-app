0567c5c5e9b1171549ed7811bfd56de1c1db5c56
Dion Garman <diongarman@gmail.com>
2019-12-08 15:03:53 +0000

    Setting up Character List - when using own subjects must unsubscribe when component gets destroyed (stage in lifecyle hook) to prevent pile up of unused subscriptions in memory

--- src/app/characters/characters.page.ts
+++ src/app/characters/characters.page.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CharactersService } from './characters.service';
 import { Character } from './character.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.page.html',
   styleUrls: ['./characters.page.scss'],
 })
-export class CharactersPage implements OnInit {
+export class CharactersPage implements OnInit, OnDestroy {
   characters: Character[];
+  private charactersSubs: Subscription;
 
   constructor(private characterService: CharactersService) { }
 
   ngOnInit() {
     this.characters = this.characterService.fetchCharacters();
-    // subscribe listens for new values
-    this.characterService.charactersChanged.subscribe(characters => {
+    this.charactersSubs = this.characterService.charactersChanged.subscribe(characters => {
       this.characters = characters;
     });
+  }
+
+  ngOnDestroy() {
+    this.charactersSubs.unsubscribe();
   }
 
 }
